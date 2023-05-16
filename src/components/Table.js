2ac1554accb96cbaa0e7de35e37a5b1237fbe554
Danillo Santos <dnashy@hotmail.com>
2023-05-16 14:22:09 -0300

    Conclui requisito 6, adicionando tabela de gastos

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Método de pagamento</th>
+            <th>Tag</th>
+            <th>Descrição</th>
+            <th>Câmbio utilizado</th>
+            <th>Moeda de conversão</th>
+            <th>Valor convertido</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody />
+      </table>
     );
   }
 }
